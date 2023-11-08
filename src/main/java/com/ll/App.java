53448f5eb46abfbfcd2e72da62b17c5a13131104
pr353nt <iamsherl.cked@gmail.com>
2023-11-08 11:16:45 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-
+    while (true){
         System.out.print("명령) ");
 
         Scanner scanner = new Scanner(System.in);
         String cmd = scanner.nextLine();
+
+        if (cmd.equals( "종료" )){
+            break;
+        }
 
         System.out.printf("입력하신 명령 : %s\n", cmd);
         // 위 라인은 아래 라인과 같은 뜻
         // System.out.print("입력하신 명령 : " + cmd + "\n");
     }
 
-}
+}}
