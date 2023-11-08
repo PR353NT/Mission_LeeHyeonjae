651d41284a45d4fcac9e37df9ea2a7356438ec29
pr353nt <iamsherl.cked@gmail.com>
2023-11-08 11:58:06 +0900

    2단계 입력

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -5,24 +5,30 @@ import java.util.Scanner;
 class App {
     void run() {
         System.out.println("== 명언 앱 ==");
-    while (true){
-        System.out.print("명령) ");
+        while (true) {
+            System.out.print("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
-        String cmd = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
+            String cmd = scanner.nextLine();
 
-        if (cmd.equals( "종료" )){
-            break;
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                System.out.print("명언: ");
+                String content = scanner.nextLine();
+
+                System.out.print("작가: ");
+                String authorName =scanner.nextLine();
+
+                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+                // System.out.print("명언 : " + content + ", 작가 : " + authorName +"\n");
+
+            }
+
+            //System.out.printf("입력하신 명령 : %s\n", cmd);
+            // 위 라인은 아래 라인과 같은 뜻
+            // System.out.print("입력하신 명령 : " + cmd + "\n");
         }
-        else if (cmd.equals( "등록" )){
-            System.out.println("명언: ");
-            System.out.println("작가: ");
 
-        }
-
-        //System.out.printf("입력하신 명령 : %s\n", cmd);
-        // 위 라인은 아래 라인과 같은 뜻
-        // System.out.print("입력하신 명령 : " + cmd + "\n");
     }
-
-}}
+}
